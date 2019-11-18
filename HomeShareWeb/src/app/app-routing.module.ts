ea81f0b13291a8e062d005a5309894d3d8d990a0
forma308 <forma308@FORMA308>
2019-11-18 10:56:47 +0100

    debut du service api + page login est installation bootstrap

--- HomeShareWeb/src/app/app-routing.module.ts
+++ HomeShareWeb/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { InscriptionComponent } from './pages/inscription/inscription.component';
+import { HomeComponent } from './pages/home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'inscription', component: InscriptionComponent },
+//  { path: 'auth', component: AuthComponent },
+   { path: '', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
